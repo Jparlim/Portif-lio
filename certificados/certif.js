635e392e2a7f2450bf0a51b1cf8e2542bf68bd84
Jparlim <Gusjao0500@gmail.com>
2024-08-02 16:16:42 -0300

    insert button x

--- certificados/certif.js
+++ certificados/certif.js
@@ -1,9 +1,20 @@
-const rotas = ['one', 'two', 'thre', 'four']
+function openimg(evento) {
+    evento = window.event || event
+    const lol = evento.target.id
+    
+    if(lol === 'one') {
+        document.querySelector('#one').classList.add('hide')
+    } if(lol === 'two') {
+        document.querySelector('#two').classList.add('hide')
+    } if(lol === 'three') {
+        document.querySelector('#three').classList.add('hide')
+    } if(lol === 'four') {
+        document.querySelector('#four').classList.add('hide')
+    }
+}
 
-function openimg() {
-    rotas.forEach( data => {
-        document.querySelector()
-    })
+function toclose() {
+    
 }
 
 window.openimg = () => openimg()
