cc1ac6efbd8d08707c30c0b8a2eb35ec6d1860db
Jparlim <Gusjao0500@gmail.com>
2024-08-08 20:21:56 -0300

    adjust certifs

--- certificados/certif.js
+++ certificados/certif.js
@@ -1,15 +1,30 @@
+
+/* ideia 1: quando o usuario clica em outro certificado e ja um ja estiver um com o flluter
+o sistema vai ver qual dos 4 certificados tem o hide, vai tira-lo e adicionar o hide no outro certificado
+que o usuario clicou*/
+
+/*ideia 2: quando o usuario clica no primeiro ceertificado o ID do certificado
+ficará salvo e quando o usuário clica em outro certificado será retirado o
+flluter do certificado 1 e colocara no certificado que o usuário clicou*/
+
+function analist(alvo) {
+    
+    
+
+}
+
+
 function openimg(evento) {
     evento = window.event || event
     const lol = evento.target.id
     
-    document.querySelector(`#${lol}`).classList.add('hide')
-
-    setTimeout(() => document.querySelector('.X').classList.add('hide'), 500);
+    analist(lol)
+    
+    document.querySelector(`#${lol}`).classList.add('imgflluter')
+    
+    setTimeout(() => document.querySelector('#X').classList.add('hide'), 500);
+    
 }
 
-function toclose() {
-    document.querySelector('.X').classList.remove('hide')
-}
 
-window.toclose = () => toclose()
 window.openimg = () => openimg()
